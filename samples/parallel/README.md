a712cc9021b4bb77206f0b7198544145cae752ad
AndreyDobrovolskyOdessa <andrey.dobrovolsky.odessa@gmail.com>
2023-07-09 19:32:45 +0300

    samples/parallel: log2map.lua utility added

--- samples/parallel/README.md
+++ samples/parallel/README.md
@@ -45,3 +45,16 @@ Starting in redo-c git directory:
     cd samples/parallel/loop
     redo parallel
 
+## Roadmap
+
+Roadmap file contains the numeric representation of the build tree along with the target names (absolute or relative). The project build log(s) may be transponded into the roadmap file with the help of log2map.lua utility.
+
+    lua log2map.lua logfile > roadmap
+
+for absolute target names or
+
+    MAP_DIR=$(pwd) lua log2map.lua logfile > roadmap
+
+to write the roadmap with the relative target names.
+
+
---
+++ samples/parallel/log2map.lua
@@ -0,0 +1,131 @@
+----------------------------------------------
+-- Capture the build graph in symbolic form --
+----------------------------------------------
+
+local node = {}
+
+local explore
+
+explore = function(dep, target)
+  for i, name in ipairs(dep) do
+    local record = dep[i + 1]
+    if type(record) == "table" then
+      if not node[name] then node[name] = {"busy"} end
+      if record.err == 0 then
+        node[name][1] = nil
+        if target then node[name][target] = true end
+        explore(record, name)
+      else
+        if (record.err & 0xff) ~= 2 then os.exit(1) end
+      end
+    end
+  end 
+end
+
+for i, logname in ipairs(arg) do
+  local f = assert(loadfile(logname))
+  explore(f())
+end
+
+
+-----------------------
+-- Enumerating nodes --
+-----------------------
+
+local dict = {}
+
+for name, staff in pairs(node) do
+  if staff[1] then os.exit(1) end  -- busy
+  dict[#dict + 1] = name
+  dict[name] = #dict
+end
+
+
+---------------------------------------
+-- Store dependences in numeric form --
+---------------------------------------
+
+local status = {}
+local children = {}
+local child = {}
+
+for i, name in ipairs(dict) do
+  status[i] = 0
+  children[i] = #child
+  for k, v in pairs(node[name]) do
+    child[#child + 1] = dict[k]
+  end
+end
+
+children[#dict + 1] = #child
+
+
+for i, j in ipairs(child) do
+  status[j] = status[j] + 1
+end
+
+
+------------------------------------
+-- Making the node names relative --
+------------------------------------
+
+map_dir = os.getenv("MAP_DIR")
+
+if map_dir then
+  map_dir = map_dir .. "/"
+
+  local find_slashes = function(s)
+    local t = {}
+    for i in s:gmatch("()/") do
+      t[#t + 1] = i
+    end
+    return t
+  end
+
+  local map_dir_slash = find_slashes(map_dir)
+
+  for i, name in ipairs(dict) do
+    local name_slash = find_slashes(name)
+
+    local eq_cnt, eq_pos
+
+    for i, map_pos in ipairs(map_dir_slash) do
+      if map_pos ~= name_slash[i] then break end
+      if map_dir:sub(1, map_pos) ~= name:sub(1, name_slash[i]) then break end
+      eq_cnt, eq_pos = i, map_pos
+    end
+
+    dict[i] = ("../"):rep(#map_dir_slash - eq_cnt) .. name:sub(eq_pos + 1, -1)
+  end
+end
+
+
+-------------------------
+-- Writing the roadmap --
+-------------------------
+
+io.write((" "):rep(#dict * 8))
+
+io.write("\n")
+
+for i, name in ipairs(dict) do
+  io.write(" ", ("%3d"):format(status[i]))
+end
+
+io.write("\n")
+
+for i, offset in ipairs(children) do
+  io.write(" ", ("%3d"):format(offset))
+end
+
+io.write("\n")
+
+for i, j in ipairs(child) do
+  io.write(" ", ("%3d"):format(j - 1))
+end
+
+for i, name in ipairs(dict) do
+  io.write("\n", name)
+end
+
+
